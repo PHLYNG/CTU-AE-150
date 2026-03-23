6bb97dd771cdb961ba9319e04e43a728b6dba9ed
Phil A <itsphilip@gmail.com>
2026-03-23 10:55:03 -0400

    Update temp_data_logging.ino

--- temp_data_logging.ino
+++ temp_data_logging.ino
@@ -1,144 +1,60 @@
-#include <Adafruit_BMP085.h>
-
-/*************************************************** 
-  This is an example for the BMP085 Barometric Pressure & Temp Sensor
-
-  Designed specifically to work with the Adafruit BMP085 Breakout 
-  ----> https://www.adafruit.com/products/391
-
-  These pressure and temperature sensors use I2C to communicate, 2 pins
-  are required to interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
-// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
-// Connect GND to Ground
-// Connect SCL to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
-// Connect SDA to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
-// EOC is not used, it signifies an end of conversion
-// XCLR is a reset pin, also not used here
-
-Adafruit_BMP085 bmp;
-  
-void setup() {
-  Serial.begin(9600);
-  if (!bmp.begin()) {
-	Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-	while (1) {}
-  }
-}
-  
-void loop() {
-    Serial.print("Temperature = ");
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-    
-    Serial.print("Pressure = ");
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-    
-    // Calculate altitude assuming 'standard' barometric
-    // pressure of 1013.25 millibar = 101325 Pascal
-    Serial.print("Altitude = ");
-    Serial.print(bmp.readAltitude());
-    Serial.println(" meters");
-
-    Serial.print("Pressure at sealevel (calculated) = ");
-    Serial.print(bmp.readSealevelPressure());
-    Serial.println(" Pa");
-
-  // you can get a more precise measurement of altitude
-  // if you know the current sea level pressure which will
-  // vary with weather and such. If it is 1015 millibars
-  // that is equal to 101500 Pascals.
-    Serial.print("Real altitude = ");
-    Serial.print(bmp.readAltitude(101500));
-    Serial.println(" meters");
-    
-    Serial.println();
-    delay(500);
-}
-/*
-  SD card datalogger
-
-  This example shows how to log data from three analog sensors
-  to an SD card using the SD library. Pin numbers reflect the default
-  SPI pins for Uno and Nano models
-
-  The circuit:
-   analog sensors on analog pins 0, 1, and 2
-   SD card attached to SPI bus as follows:
- ** SDO - pin 11
- ** SDI - pin 12
- ** CLK - pin 13
- ** CS - depends on your SD card shield or module.
- 		Pin 10 used here for consistency with other Arduino examples
-    (for MKR Zero SD: SDCARD_SS_PIN)
-
-  created  24 Nov 2010
-  modified  24 July 2020
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-*/
-
 #include <SPI.h>
 #include <SD.h>
+#include <Adafruit_BMP085.h>
 
-const int chipSelect = 10;
+// PIN CONFIGURATION
+const int chipSelect = 10; // Ensure this matches your wiring
+Adafruit_BMP085 bmp;
 
 void setup() {
-  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  // wait for Serial Monitor to connect. Needed for native USB port boards only:
-  while (!Serial);
+  while (!Serial); // Wait for Serial Monitor
 
-  Serial.print("Initializing SD card...");
+  Serial.println("Initializing System...");
 
-  if (!SD.begin(chipSelect)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("1. is a card inserted?");
-    Serial.println("2. is your wiring correct?");
-    Serial.println("3. did you change the chipSelect pin to match your shield or module?");
-    Serial.println("Note: press reset button on the board and reopen this Serial Monitor after fixing your issue!");
-    while (true);
+  // 1. Initialize BMP085
+  if (!bmp.begin()) {
+    Serial.println("Could not find BMP085. Check wiring (Pins 20/21)!");
+    while (1);
   }
 
-  Serial.println("initialization done.");
+  // 2. Initialize SD Card
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("SD initialization failed! Check wiring (Pins 50-52).");
+    return; // Don't proceed if SD fails
+  }
+  Serial.println("SD initialization done.");
+
+  // Create Header for CSV file if it doesn't exist
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  if (dataFile) {
+    dataFile.println("Temp(C),Pressure(Pa),Altitude(m)");
+    dataFile.close();
+  }
 }
 
 void loop() {
-  // make a string for assembling the data to log:
-  String dataString = "";
+  // Read sensor data
+  float temp = bmp.readTemperature();
+  int32_t press = bmp.readPressure();
+  float alt = bmp.readAltitude();
 
-  // read three sensors and append to the string:
-  for (int analogPin = 0; analogPin < 3; analogPin++) {
-    int sensor = analogRead(analogPin);
-    dataString += String(sensor);
-    if (analogPin < 2) {
-      dataString += ",";
-    }
-  }
+  // Create a comma-separated string
+  String dataString = String(temp) + "," + String(press) + "," + String(alt);
 
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
+  // Log to SD Card
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
-
-  // if the file is available, write to it:
   if (dataFile) {
     dataFile.println(dataString);
     dataFile.close();
-    // print to the serial port too:
+    
+    // Also print to Serial for debugging
+    Serial.print("Logged: ");
     Serial.println(dataString);
+  } else {
+    Serial.println("Error opening datalog.txt");
   }
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening datalog.txt");
-  }
+
+  delay(2000); // Log every 2 seconds
 }
