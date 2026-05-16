824de8d029e9aaa68c6fd60127076f5258ef3126
Phil A <itsphilip@gmail.com>
2026-05-16 16:58:00 -0400

    Update SD_payload_1.ino

--- SD_payload_1.ino
+++ SD_payload_1.ino
@@ -1,217 +1,157 @@
-/**************************************************************************
-   Tests the getTemperature and getHumidity functions of the Qwiic Humidity
-   library
-
-   Priyanka Makin @ SparkFun Electronics
-   Original Creation Date: March 31, 2020
-
-   SparkFun labored with love to create this code. Feel like supporting open
-   source hardware? Buy a board from SparkFun! https://www.sparkfun.com/products/16618
-
-   This code is lemonadeware; if you see me (or any other SparkFun employee)
-   at the local, and you've found our code helpful, please buy us a round!
-
-   Hardware Connections:
-   Attach a RedBoard to computer using micro-B USB cable.
-   Attach a Qwiic Humidity board to RedBoard using Qwiic cable.
-
-   Distributed as-is; no warranty is given.
- **************************************************************************/
 #include <Wire.h>
-
-#include <SparkFun_Qwiic_Humidity_AHT20.h> //Click here to get the library: http://librarymanager/All#Qwiic_Humidity_AHT20 by SparkFun
-AHT20 humiditySensor;
-
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println("Qwiic Humidity AHT20 examples");
-
-  Wire.begin(); //Join I2C bus
-
-  //Check if the AHT20 will acknowledge
-  if (humiditySensor.begin() == false)
-  {
-    Serial.println("AHT20 not detected. Please check wiring. Freezing.");
-    while (1);
-  }
-  Serial.println("AHT20 acknowledged.");
-}
-
-void loop()
-{
-  //If a new measurement is available
-  if (humiditySensor.available() == true)
-  {
-    //Get the new temperature and humidity value
-    float temperature = humiditySensor.getTemperature();
-    float humidity = humiditySensor.getHumidity();
-
-    //Print the results
-    Serial.print("Temperature: ");
-    Serial.print(temperature, 2);
-    Serial.print(" C\t");
-    Serial.print("Humidity: ");
-    Serial.print(humidity, 2);
-    Serial.print("% RH");
-
-    Serial.println();
-  }
-
-  //The AHT20 can respond with a reading every ~50ms. However, increased read time can cause the IC to heat around 1.0C above ambient.
-  //The datasheet recommends reading every 2 seconds.
-  delay(2000);
-}
-
-/* this is example code for the ADXL345 accelerometer
-i have confirmed that it works on a breadboard setup */
-
-#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <RTClib.h>
 #include <Adafruit_Sensor.h>
+#include <Adafruit_AHTX0.h>
 #include <Adafruit_ADXL345_U.h>
+#include <MS5611.h>
 
-/* Assign a unique ID to this sensor at the same time */
+// --- HARDWARE CONSTANTS ---
+const int chipSelect = 5;
+
+char filename[20];
+float launchPadPressure; // Will be stored in Pascals
+
+// --- SENSOR OBJECTS ---
+RTC_DS3231 rtc;
+Adafruit_AHTX0 aht;
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+MS5611 ms5611;
 
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  accel.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");  
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
+// --- DEFENSIVE STATUS FLAGS ---
+bool rtcOk = false, ahtOk = false, accelOk = false, msOk = false;
 
-void displayDataRate(void)
-{
-  Serial.print  ("Data Rate:    "); 
-  
-  switch(accel.getDataRate())
-  {
-    case ADXL345_DATARATE_3200_HZ:
-      Serial.print  ("3200 "); 
-      break;
-    case ADXL345_DATARATE_1600_HZ:
-      Serial.print  ("1600 "); 
-      break;
-    case ADXL345_DATARATE_800_HZ:
-      Serial.print  ("800 "); 
-      break;
-    case ADXL345_DATARATE_400_HZ:
-      Serial.print  ("400 "); 
-      break;
-    case ADXL345_DATARATE_200_HZ:
-      Serial.print  ("200 "); 
-      break;
-    case ADXL345_DATARATE_100_HZ:
-      Serial.print  ("100 "); 
-      break;
-    case ADXL345_DATARATE_50_HZ:
-      Serial.print  ("50 "); 
-      break;
-    case ADXL345_DATARATE_25_HZ:
-      Serial.print  ("25 "); 
-      break;
-    case ADXL345_DATARATE_12_5_HZ:
-      Serial.print  ("12.5 "); 
-      break;
-    case ADXL345_DATARATE_6_25HZ:
-      Serial.print  ("6.25 "); 
-      break;
-    case ADXL345_DATARATE_3_13_HZ:
-      Serial.print  ("3.13 "); 
-      break;
-    case ADXL345_DATARATE_1_56_HZ:
-      Serial.print  ("1.56 "); 
-      break;
-    case ADXL345_DATARATE_0_78_HZ:
-      Serial.print  ("0.78 "); 
-      break;
-    case ADXL345_DATARATE_0_39_HZ:
-      Serial.print  ("0.39 "); 
-      break;
-    case ADXL345_DATARATE_0_20_HZ:
-      Serial.print  ("0.20 "); 
-      break;
-    case ADXL345_DATARATE_0_10_HZ:
-      Serial.print  ("0.10 "); 
-      break;
-    default:
-      Serial.print  ("???? "); 
-      break;
-  }  
-  Serial.println(" Hz");  
-}
+void setup() {
+  Serial.begin(115200);
+  unsigned long startWait = millis();
+  while (!Serial && millis() - startWait < 3000);
 
-void displayRange(void)
-{
-  Serial.print  ("Range:         +/- "); 
-  
-  switch(accel.getRange())
-  {
-    case ADXL345_RANGE_16_G:
-      Serial.print  ("16 "); 
-      break;
-    case ADXL345_RANGE_8_G:
-      Serial.print  ("8 "); 
-      break;
-    case ADXL345_RANGE_4_G:
-      Serial.print  ("4 "); 
-      break;
-    case ADXL345_RANGE_2_G:
-      Serial.print  ("2 "); 
-      break;
-    default:
-      Serial.print  ("?? "); 
-      break;
-  }  
-  Serial.println(" g");  
-}
+  Serial.println("\n--- FULL MISSION INITIALIZATION ---");
+  Wire.begin();
+  Wire.setTimeOut(50); // Prevent I2C lockups
 
-void setup(void) 
-{
-  Serial.begin(9600);
-  Serial.println("Accelerometer Test"); Serial.println("");
-  
-  /* Initialise the sensor */
-  if(!accel.begin())
-  {
-    /* There was a problem detecting the ADXL345 ... check your connections */
-    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
-    while(1);
+  // 1. RTC SYNC
+  if (rtc.begin()) {
+    rtcOk = true;
+    if (rtc.lostPower()) rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    Serial.println("[OK] RTC Online");
+  } else {
+    Serial.println("[FAIL] RTC Offline");
   }
 
-  /* Set the range to whatever is appropriate for your project */
-  accel.setRange(ADXL345_RANGE_16_G);
-  // accel.setRange(ADXL345_RANGE_8_G);
-  // accel.setRange(ADXL345_RANGE_4_G);
-  // accel.setRange(ADXL345_RANGE_2_G);
+  // 2. SD CARD (Critical Fail)
+  if (!SD.begin(chipSelect)) {
+    Serial.println("CRITICAL FAIL: SD Card");
+    while (1); // Halt execution if SD fails
+  }
+
+  int fileCount = 0;
+  sprintf(filename, "/log%d.csv", fileCount);
+  while (SD.exists(filename)) { 
+    sprintf(filename, "/log%d.csv", ++fileCount); 
+  }
+  Serial.printf("File: %s\n", filename);
+
+  // 3. SENSOR INIT & CALIBRATION
   
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-  
-  /* Display additional settings (outside the scope of sensor_t) */
-  displayDataRate();
-  displayRange();
-  Serial.println("");
+  // AHT20 Init
+  if (aht.begin()) {
+    ahtOk = true;
+    Serial.println("[OK] AHT20 Online");
+  } else {
+    Serial.println("[FAIL] AHT20 Offline");
+  }
+
+  // ADXL345 Init
+  if (accel.begin()) {
+    accelOk = true;
+    accel.setRange(ADXL345_RANGE_16_G); // Best for high dynamic environments like launches
+    Serial.println("[OK] ADXL345 Online");
+  } else {
+    Serial.println("[FAIL] ADXL345 Offline");
+  }
+
+  // MS5611 Init & Calibration
+  if (ms5611.begin()) {
+    msOk = true;
+    float totalP = 0;
+    // Take average of 5 readings to establish ground baseline
+    for(int i = 0; i < 5; i++) { 
+      ms5611.read();
+      // ms5611.getPressure() returns millibar (mbar). Multiply by 100 for Pascals.
+      totalP += (ms5611.getPressure() * 100.0); 
+      delay(50); 
+    }
+    launchPadPressure = totalP / 5.0;
+    Serial.println("[OK] MS5611 Online");
+  } else {
+    Serial.println("[FAIL] MS5611 Offline");
+  }
+
+  // 4. HEADER WRITING
+  File dataFile = SD.open(filename, FILE_WRITE);
+  if (dataFile) {
+    dataFile.println("Millis,Time,Temp_C,Humidity_%,Press_Pa,Alt_m,AccelX_ms2,AccelY_ms2,AccelZ_ms2");
+    dataFile.close();
+  }
 }
 
-void loop(void) 
-{
-  /* Get a new sensor event */ 
-  sensors_event_t event; 
-  accel.getEvent(&event);
- 
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
-  delay(500);
+void loop() {
+  // --- A. ENVIRONMENTAL DATA (AHT20) ---
+  float temp = -999, humidity = -999;
+  if (ahtOk) {
+    sensors_event_t aht_humidity, aht_temp;
+    aht.getEvent(&aht_humidity, &aht_temp);
+    temp = aht_temp.temperature;
+    humidity = aht_humidity.relative_humidity;
+  }
+
+  // --- B. KINEMATIC DATA (ADXL345) ---
+  float ax = 0, ay = 0, az = 0;
+  if (accelOk) {
+    sensors_event_t event;
+    accel.getEvent(&event);
+    ax = event.acceleration.x;
+    ay = event.acceleration.y;
+    az = event.acceleration.z;
+  }
+
+  // --- C. PRESSURE & ALTITUDE (MS5611) ---
+  float press = 0, alt = -999;
+  if (msOk) {
+    ms5611.read();
+    press = ms5611.getPressure() * 100.0; // Convert mbar to Pa
+    
+    // Standard barometric formula for altitude based on launchpad baseline
+    if (launchPadPressure > 0) {
+      alt = 44330.0 * (1.0 - pow(press / launchPadPressure, 0.1903));
+    }
+  }
+
+  // --- D. TIME ---
+  String timeStr = "00:00:00";
+  if (rtcOk) {
+    DateTime now = rtc.now();
+    char buf[10];
+    sprintf(buf, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
+    timeStr = String(buf);
+  }
+
+  // --- E. LOGGING ---
+  String dataLine = String(millis()) + "," + timeStr + "," + 
+                    String(temp) + "," + String(humidity) + "," + 
+                    String(press) + "," + String(alt) + "," +
+                    String(ax) + "," + String(ay) + "," + String(az);
+
+  File dataFile = SD.open(filename, FILE_APPEND); // Note: Older SD libs use FILE_WRITE. Using FILE_APPEND as requested in your snippet.
+  if (dataFile) {
+    dataFile.println(dataLine);
+    dataFile.close();
+    Serial.print("LOGGED: "); Serial.println(dataLine);
+  } else {
+    Serial.println("!! SD ERROR !!");
+  }
+
+  delay(2000);
 }
